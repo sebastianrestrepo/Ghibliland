ffa226d34c3ceb32825289fbda72a53ae28758c6
sebastianrestrepo <sebastianrestrepoq@gmail.com>
2017-04-25 23:45:16 -0500

    Se inician a cargar las pantallas

--- GarcíaPaulaRestrepoSebastian_Taller3/src/MainAppTaller3.java
+++ GarcíaPaulaRestrepoSebastian_Taller3/src/MainAppTaller3.java
@@ -10,31 +10,32 @@ public class MainAppTaller3 extends PApplet {
 
 	private Mundo m;
 
-	//hey!
-	//:3
-	
+	// hey!
+	// :3
+
 	public static void main(String[] args) {
 		PApplet.main("MainAppTaller3");
 	}
 
 	@Override
 	public void settings() {
-
+		size(700, 700);
 	}
 
 	@Override
 	public void setup() {
 		m = new Mundo(this);
+		m.cargarImagenes();
 	}
 
 	@Override
 	public void draw() {
-
+		m.pantallas();
 	}
 
 	@Override
 	public void keyReleased() {
-
+		m.teclas();
 	}
 
 }
--- GarcíaPaulaRestrepoSebastian_Taller3/src/Mundo.java
+++ GarcíaPaulaRestrepoSebastian_Taller3/src/Mundo.java
@@ -1,32 +1,39 @@
 import java.util.ArrayList;
-
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Mundo {
 
 	private PApplet app;
+	private int pantallas;
+	private PImage fondoInicio, instrucciones;
 	private ArrayList<Criatura> criaturas;
 	private ArrayList<Comida> comida;
 	private GatoBus gatobus;
 
 	/*
-	 *  Constructor de Mundo 
-	 *  @parametro PApplet app
+	 * Constructor de Mundo
+	 * 
+	 * @parametro PApplet app
 	 */
 	public Mundo(PApplet app) {
 		this.app = app;
+		app.imageMode(app.CENTER);
 	}
 
 	/*
 	 * Método que se encargará de cargar las imagenes de las pantallas
+	 * 
 	 * @retorno void
 	 */
 	public void cargarImagenes() {
-
+		fondoInicio = app.loadImage("fondoInicio.png");
+		instrucciones = app.loadImage("instrucciones.png");
 	}
 
 	/*
 	 * Método que se encargará de iniciar todas las variables y listas
+	 * 
 	 * @retorno void
 	 */
 	public void iniciarVariables() {
@@ -35,6 +42,7 @@ public class Mundo {
 
 	/*
 	 * Método que se encargará de iniciar y encapsular los Hilos
+	 * 
 	 * @retorno void
 	 */
 	public void iniciarHilos() {
@@ -44,6 +52,7 @@ public class Mundo {
 	/*
 	 * Método que se encargará de llamar a todos los pintar que vienen de las
 	 * otras clases y que será llamado en el Ejectuable en el draw
+	 * 
 	 * @retorno void
 	 */
 	public void pintar() {
@@ -51,25 +60,54 @@ public class Mundo {
 	}
 
 	/*
-	 * Método que se encargará de los eventos del teclado
-	 * otras clases y que será llamado en el Ejectuable en el keyReleased
+	 * Método que se encargará de los eventos del teclado otras clases y que
+	 * será llamado en el Ejectuable en el keyReleased
+	 * 
 	 * @retorno void
 	 */
 	public void teclas() {
-
+		switch (pantallas) {
+		case 0:
+			if(app.keyCode == app.ENTER){
+				pantallas = 1;
+			}
+			break;
+		case 1:
+			if(app.keyCode == app.ENTER){
+				pantallas = 2;
+			}
+			break;
+		case 2:
+			app.background(0);
+			break;
+		}
 	}
 
 	/*
 	 * Método que se contendrá un switch que definirá los cambios pantallas
+	 * 
 	 * @retorno void
 	 */
 	public void pantallas() {
-
+		switch (pantallas) {
+		case 0:
+			app.image(fondoInicio, app.width/2, app.height/2);
+			break;
+		case 1:
+			app.image(fondoInicio, app.width/2, app.height/2);
+			app.image(instrucciones, app.width/2, app.height/2);
+			break;
+		case 2:
+			app.background(0);
+			break;
+		}
 	}
 
 	/*
-	 * Método que se removerá del ArrayList al elementos que esté huyendo justo en el momento 
-	 * en que salda de la pantalla, lo sabrá por medio del boolean 'huyendo'.
+	 * Método que se removerá del ArrayList al elementos que esté huyendo justo
+	 * en el momento en que salda de la pantalla, lo sabrá por medio del boolean
+	 * 'huyendo'.
+	 * 
 	 * @retorno void
 	 */
 	public void remover() {
